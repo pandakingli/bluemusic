7e0ebffa179271911bb203941fb36bcdeafa9d62
biubiublue <utopia.pro@aliyun.com>
2018-07-04 10:28:16 +0800

    fix

--- blueMusic/Helper/MusicPlayerHandle.h
+++ blueMusic/Helper/MusicPlayerHandle.h
@@ -1,6 +1,6 @@
 //
 //  MusicPlayer.h
-//  MusicPlay1102
+//  
 //
 //  Created by biubiu on 15/11/3.
 //  Copyright © 2015年 lining. All rights reserved.
@@ -20,19 +20,19 @@ typedef void(^musicPlayTimeBlock) (float);
 
 
 @interface MusicPlayerHandle : NSObject
-//代理使用assign，防止父类对象作为子类对象代理人的时候造成循环引用
+
++(instancetype)shareMusicPlayerHandle;
+
 @property(nonatomic,assign) id<MusicPlayerHandleDelegate> delegate;
 
 @property(nonatomic,assign) NSInteger playStatus;
 
 @property(nonatomic,assign) NSInteger index;
-//当前播放的歌曲
 
+//当前播放的歌曲
 @property(nonatomic,strong) MusicModel *nowModel;
 
 @property(nonatomic,copy) musicPlayTimeBlock mptblock;
-+(instancetype)shareMusicPlayerHandle;
-//+(instancetype)shareMusicPlayerHandleWithFinishBlock:(finishBlock)finishblock;
 
 //开始播放
 -(void)playWithURLString:(NSString*)urlString;
@@ -51,7 +51,5 @@ typedef void(^musicPlayTimeBlock) (float);
 
 //获取播放到的时间
 - (void)seekToTime:(float)time;
-
-
 
 @end
--- blueMusic/Helper/MusicPlayerHandle.m
+++ blueMusic/Helper/MusicPlayerHandle.m
@@ -1,6 +1,6 @@
 //
 //  MusicPlayer.m
-//  MusicPlay1102
+//
 //
 //  Created by biubiu on 15/11/3.
 //  Copyright © 2015年 lining. All rights reserved.
@@ -8,92 +8,95 @@
 
 #import "MusicPlayerHandle.h"
 #import <AVFoundation/AVFoundation.h>
+
 static MusicPlayerHandle *myMusicPlayer=nil;
+
 @interface MusicPlayerHandle()
-
 @property (nonatomic, strong) AVPlayer *player;
-
 @property (nonatomic, assign) BOOL isPlaying;
-
 @property (nonatomic, strong) NSTimer *timer;
 @end
 
 @implementation MusicPlayerHandle
 
-
-
-+(instancetype)shareMusicPlayerHandle{
-    if (myMusicPlayer==nil) {
++(instancetype)shareMusicPlayerHandle
+{
+    if (myMusicPlayer==nil)
+    {
         myMusicPlayer =[[MusicPlayerHandle alloc]init];
         myMusicPlayer.player =[AVPlayer new];
-        
     }
     return myMusicPlayer;
 }
 
-//+(instancetype)shareMusicPlayerHandleWithFinishBlock:(finishBlock)finishblock{
-//    if (myMusicPlayer==nil) {
-//        myMusicPlayer =[[MusicPlayerHandle alloc]init];
-//        myMusicPlayer.player =[AVPlayer new];
-//        finishblock();
-//    }
-//    return myMusicPlayer;
-//}
-
-
-
--(void)playWithURLString:(NSString*)urlString{
+#pragma mark- 播放操作
+-(void)playWithURLString:(NSString*)urlString
+{
     NSURL *url = [NSURL URLWithString:urlString];
-    //
+
     AVPlayerItem *item = [[AVPlayerItem alloc] initWithURL:url];
     [self.player replaceCurrentItemWithPlayerItem:item];
    
     [self play];
 }
--(void)play{
-    [self.player play];
-    self.isPlaying = YES;
-    
-    if (self.timer == nil) {
-        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(timerAction) userInfo:nil repeats:YES];
-        
-        [self.timer fire];
-    }
-}
-#pragma mark- timerAction
 
-- (void)timerAction
+-(void)pause
 {
-
-    // 因为CMTime的结构体已经告诉了。 time.value / time.timescale 可以得到秒数，所以，我们这里通过这个方法计算出来当前播放到多少秒了。
-            float progress = self.player.currentTime.value / self.player.currentTime.timescale;
-    self.mptblock(progress);
-}
-
--(void)pause{
     [self.player pause];
     self.isPlaying = NO;
     [self.timer invalidate];
     self.timer = nil;
 }
--(void)stop{
+-(void)stop
+{
     [self pause];
-
-}
--(BOOL)isPlaying{
-     return _isPlaying;
+    
 }
 
+-(BOOL)isPlaying
+{
+    return _isPlaying;
+}
+
+-(void)play
+{
+    [self.player play];
+    self.isPlaying = YES;
+    
+    if (self.timer == nil)
+    {
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.02
+                                                      target:self
+                                                    selector:@selector(timerAction)
+                                                    userInfo:nil
+                                                     repeats:YES];
+        [self.timer fire];
+    }
+}
+
+#pragma mark- timerAction
+- (void)timerAction
+{
+    // time.value / time.timescale计算出来当前播放到多少秒了。
+    CMTimeValue current = self.player.currentTime.value;
+    CMTimeValue total   = self.player.currentTime.timescale;
+    float progress = current / total;
+    self.mptblock(progress);
+}
+
+#pragma mark- 定位到某个时间点
 - (void)seekToTime:(float)time
 {
     CMTime musicTime = CMTimeMakeWithSeconds(time, self.player.currentTime.timescale);
     
     [self pause];//先暂停
     
-    [self.player seekToTime:musicTime completionHandler:^(BOOL finished)
-    
-    {if (finished) { [self play];}  }
-     ];
+    [self.player seekToTime:musicTime completionHandler:^(BOOL finished){
+        if (finished)
+        {
+            [self play];
+        }
+    }];
 }
 
 @end
