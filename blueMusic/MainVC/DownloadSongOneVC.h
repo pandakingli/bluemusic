395c81f69f9822fac113f553212874e1ab8da774
mac2015 <utopia.pro@aliyun.com>
2018-07-12 22:02:11 +0800

    fix

---
+++ blueMusic/MainVC/DownloadSongOneVC.h
@@ -0,0 +1,13 @@
+//
+//  DownloadSongOneVC.h
+//  blueMusic
+//
+//  Created by lining on 2018/7/12.
+//  Copyright © 2018年 biubiublue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class MusicModel;
+@interface DownloadSongOneVC : UIViewController
+-(void)godownloadmodel:(MusicModel*)mmodel;
+@end
---
+++ blueMusic/MainVC/DownloadSongOneVC.m
@@ -0,0 +1,28 @@
+//
+//  DownloadSongOneVC.m
+//  blueMusic
+//
+//  Created by lining on 2018/7/12.
+//  Copyright © 2018年 biubiublue. All rights reserved.
+//
+
+#import "DownloadSongOneVC.h"
+#import "MusicModel.h"
+
+@interface DownloadSongOneVC ()
+
+@end
+
+@implementation DownloadSongOneVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+-(void)godownloadmodel:(MusicModel*)mmodel
+{
+    
+}
+
+@end
