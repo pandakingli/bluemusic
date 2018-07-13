e02123e693d1a6775097b58f2d64617df3051760
mac2015 <utopia.pro@aliyun.com>
2018-07-13 09:27:08 +0800

    fix

---
+++ blueMusic/ViewController/localMusicVC.h
@@ -0,0 +1,13 @@
+//
+//  localMusicVC.h
+//  blueMusic
+//
+//  Created by lining on 2018/7/13.
+//  Copyright © 2018年 biubiublue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface localMusicVC : UIViewController
+
+@end
---
+++ blueMusic/ViewController/localMusicVC.m
@@ -0,0 +1,37 @@
+//
+//  localMusicVC.m
+//  blueMusic
+//
+//  Created by lining on 2018/7/13.
+//  Copyright © 2018年 biubiublue. All rights reserved.
+//
+
+#import "localMusicVC.h"
+
+@interface localMusicVC ()
+
+@end
+
+@implementation localMusicVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
